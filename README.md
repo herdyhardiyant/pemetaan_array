55e1ddb7948d658ccd85efea59b84c7f66614d15
Herdy <herdyhardiyant@gmail.com>
2021-04-19 18:39:57 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# pemetaan_array
+
+isi app
+1. pemetaan array 1 dimensi
+2. pemetaan array 2 dimensi
+3. pemetaan array 3 dimensi
---
+++ android/app/src/main/kotlin/com/example/pemetaan_array/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.pemetaan_array
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
