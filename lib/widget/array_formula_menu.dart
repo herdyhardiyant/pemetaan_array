e576b14c93cd8d8a5523bfdf3902d33166c3c28d
Herdy <herdyhardiyant@gmail.com>
2021-04-21 23:06:58 +0700

    ready to be tested

--- lib/widget/array_formula_menu.dart
+++ lib/widget/array_formula_menu.dart
@@ -6,41 +6,44 @@ import 'package:pemetaan_array/widget/array_3d.dart';
 
 class ArrayFormulaMenu extends StatelessWidget {
   
-  static const _spacesBetweenElementsInsideColumn = 40.00;
+  static const _spacesBetweenElementsInsideColumn = 100.00;
   
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        margin: EdgeInsets.all(40),
-        child: Column(
-          children: [
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Array1D()));
-                },
-                child: Text("Hitung Array 1 Dimensi")),
-            SizedBox(
-              height: _spacesBetweenElementsInsideColumn,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Array2D()));
-                },
-                
-                child: Text("Hitung Array 2 Dimensi")),
-            SizedBox(
-              height: _spacesBetweenElementsInsideColumn,
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Array3D()));
-                },
-                child: Text("Hitung Array 3 Dimensi")),
-          ],
+        margin: EdgeInsets.all(_spacesBetweenElementsInsideColumn),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Array1D()));
+                  },
+                  child: Text("Hitung Array 1 Dimensi"),
+              ),
+              SizedBox(
+                height: _spacesBetweenElementsInsideColumn,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Array2D()));
+                  },
+                  
+                  child: Text("Hitung Array 2 Dimensi")),
+              SizedBox(
+                height: _spacesBetweenElementsInsideColumn,
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => Array3D()));
+                  },
+                  child: Text("Hitung Array 3 Dimensi")),
+            ],
+          ),
         ),
       ),
     );
