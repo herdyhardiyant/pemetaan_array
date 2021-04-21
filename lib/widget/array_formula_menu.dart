d49e53364e1f24bea5ac0c184681fabc3c0ffd67
Herdy <herdyhardiyant@gmail.com>
2021-04-21 23:02:57 +0700

    ready to use

--- lib/widget/array_formula_menu.dart
+++ lib/widget/array_formula_menu.dart
@@ -5,11 +5,14 @@ import 'package:pemetaan_array/widget/array_2d.dart';
 import 'package:pemetaan_array/widget/array_3d.dart';
 
 class ArrayFormulaMenu extends StatelessWidget {
+  
+  static const _spacesBetweenElementsInsideColumn = 40.00;
+  
   @override
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        margin: EdgeInsets.all(20),
+        margin: EdgeInsets.all(40),
         child: Column(
           children: [
             ElevatedButton(
@@ -19,16 +22,17 @@ class ArrayFormulaMenu extends StatelessWidget {
                 },
                 child: Text("Hitung Array 1 Dimensi")),
             SizedBox(
-              height: 20,
+              height: _spacesBetweenElementsInsideColumn,
             ),
             ElevatedButton(
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: (context) => Array2D()));
                 },
+                
                 child: Text("Hitung Array 2 Dimensi")),
             SizedBox(
-              height: 20,
+              height: _spacesBetweenElementsInsideColumn,
             ),
             ElevatedButton(
                 onPressed: () {
